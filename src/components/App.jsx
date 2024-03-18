a4994ad494319b7904c633d6e4c8c6e72ff3af32
Alba Melchor Gomez <albamelchorgomez@gmail.com>
2024-03-18 11:23:37 +0100

    dice functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,49 +2,54 @@
 import { useState } from "react";
 import Header from "./Header";
 import Board from "./Board";
+import Dice from "./Dice";
 import '../scss/App.scss';
-
 
 //import groguImg from '../images/grogu.webp';
 
 function App() {
 
-  const [countValue, setCountValue] = useState(1);
+  let [countValue, setCountValue] = useState(1);
   const [numberDice, setNumberDice] = useState(0);
 
-  const [eggs, setEggs] = useState(["🥚","🥚","🥚"]);
-  const [cookies, setCookies] = useState(["🍪","🍪","🍪"]);
-  const [frog, setFrog] = useState(["🐸","🐸","🐸"]);
-  
+  const [eggs, setEggs] = useState(["🥚", "🥚", "🥚"]);
+  const [cookies, setCookies] = useState(["🍪", "🍪", "🍪"]);
+  const [frog, setFrog] = useState(["🐸", "🐸", "🐸"]);
+
+
+
   function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min)
   }
-  
-  rollDice();
-  
-  function rollDice(){
+
+
+
+  function rollDice() {
     let randomNumberDice = getRandomInt(1, 4);
     console.log(randomNumberDice);
-    switch(randomNumberDice){
+    switch (randomNumberDice) {
       case randomNumberDice = 1:
         console.log('Eliminamos Huevos');
+
         break;
       case randomNumberDice = 2:
         console.log('Eliminamos Cookies');
-        break;  
+        break;
       case randomNumberDice = 3:
         console.log('Eliminamos Ranas');
         break;
       case randomNumberDice = 4:
         console.log('Grogu avanza');
+        setCountValue(countValue++);
+        console.log(countValue);
+
         break;
     }
-    
   }
 
-  
-
-
+  function handleClick(event) {
+    rollDice(event);
+  }
 
   return (
     <>
@@ -53,7 +58,8 @@ function App() {
         <Board />
 
         <section>
-          <button className="dice">Lanzar Dado</button>
+          {/*<button className="dice">Lanzar Dado</button>*/}
+          <Dice onClickButton={handleClick} />
           <div className="game-status">En curso</div>
         </section>
 
---
+++ src/components/Dice.jsx
@@ -0,0 +1,9 @@
+const Dice = (props) => {
+
+    return (
+        <div className="dice"
+            onClick={props.onClickButton}>🎲</div>
+    )
+}
+
+export default Dice;
