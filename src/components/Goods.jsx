285c04ebeaa8ea13c32afbf6b25b1fb6f07e73e7
noeliaariza <noelia.ariza@gmail.com>
2024-03-22 10:52:31 +0100

    exercise of the day

---
+++ src/components/Goods.jsx
@@ -0,0 +1,10 @@
+function Goods({ goods }) {
+  const goodsList = goods.map((good, index) => (
+    <div className="goods-item" key={index}>
+      {good}
+    </div>
+  ));
+  return [goodsList];
+}
+
+export default Goods;
